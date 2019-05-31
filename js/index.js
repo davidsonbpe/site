f44576bb36f9c54457e5f66d756d6cada6994258
Davidson Bpe <davidsomixer@gmail.com>
2019-05-31 18:57:26 -0300

    1.2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', (e) => {
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+    }
+});
+
 // Optional: This just makes all the links open in a new tab
 var links = document.links;
 for (var i = 0; i < links.length; i++) {
-     links[i].target = "_blank";
+    links[i].target = "_blank";
 }
